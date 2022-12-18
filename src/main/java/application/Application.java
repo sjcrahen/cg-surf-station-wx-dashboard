6ea6eeff46d871a61867ec45e08775eff3fc237f
shawn j crahen <shawn.j.crahen@gmail.com>
2022-12-17 18:11:27 -0600

    reverted to no login - plan to convert project to service api only

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -12,14 +12,13 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 public class Application {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder builder) {
-    return builder.build();
-  }
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
 }
--- src/main/java/application/core/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package application.core.user;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<UserEntity, String> {
-    UserEntity findByEmail(String email);
-}

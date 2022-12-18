1741fbaa899b6d813ab2b122d4a624cca661e610
shawn j crahen <shawn.j.crahen@gmail.com>
2022-12-17 18:38:24 -0600

    corrected pom

--- src/main/java/application/core/station/StationRepository.java
+++ src/main/java/application/core/station/StationRepository.java
@@ -1,7 +1,9 @@
 package application.core.station;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StationRepository extends CrudRepository<StationEntity, Long> {
-  StationEntity findByStationId(String stationId);
+    StationEntity findByStationId(String stationId);
 }
